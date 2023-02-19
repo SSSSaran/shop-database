04dd6a9e0f2051b4cc1088f921070b668288a4a4
SSSSaran <ssaran@imgroup.co.uk>
2023-02-19 20:09:42 +0000

    Update information.php

--- information.php
+++ information.php
@@ -1,24 +1,34 @@
-<?php //ctrl shift p ?>
+<?php 
 
-<?php include 'db.php' ?>
-
-<?php
+// Connect to the database is stored within $con
+// localhost = serer, username = root, password = "Prabjot!23", DBname = "shop"
 
 $con=mysqli_connect("localhost","root","Prabjot!23","shop");
 
+
+// Checking connection
+// If it doesn't connect to the database, it will show this error = "Failed to conect to MySQL"
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   exit();
 }
 
+$sqlone = "UPDATE shop SET name = "bob" WHERE name = 'demo'";
+mysqli_query($con, $sqlone);
+
+
 $sql = "SELECT name, id FROM shop ORDER BY id";
+
+// Will store name, id e.g. SQL query as a array
 $result = mysqli_query($con,$sql);
 
-// Numeric array
+
+// This will fetch the array, and is stored in $row
+// This takes two parameters which is $result, MYSQLI_NUM
 $row = mysqli_fetch_array($result, MYSQLI_NUM);
 printf ("%s (%s)\n", $row[0], $row[1]);
 
-// Associative array
+// Same this as above except you type in name and id
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 printf ("%s (%s)\n", $row["name"], $row["id"]);
 
