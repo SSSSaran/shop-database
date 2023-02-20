aaed6a54da15cafeaefa1143b250c2b587c83879
SSSSaran <125583108+SSSSaran@users.noreply.github.com>
2023-02-20 15:42:04 +0000

    Update databaseupdate.php

--- databaseupdate.php
+++ databaseupdate.php
@@ -2,7 +2,7 @@
 
 $con=mysqli_connect("localhost","root","Prabjot","shop");
 
-$sql = "UPDATE `shop` SET `name`='steve' WHERE 'id' = '0'";
+$sql = "UPDATE `shop` SET `id`= '5' WHERE 'name' = 'gary'";
 
 mysqli_query($con,$sql); 
 
