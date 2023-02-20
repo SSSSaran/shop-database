561c273d286c282d75dd1fccfceb1999684d8293
SSSSaran <125583108+SSSSaran@users.noreply.github.com>
2023-02-20 15:40:16 +0000

    Update databaseupdate.php

--- databaseupdate.php
+++ databaseupdate.php
@@ -3,10 +3,6 @@
 $con=mysqli_connect("localhost","root","Prabjot","shop");
 
 $sql = "UPDATE `shop` SET `name`='steve' WHERE 'id' = '0'";
-$sql = "UPDATE `shop` SET `id`='1' WHERE name = 'bob'";
-$sql = "UPDATE `shop` SET `id`='2' WHERE name = 'alan'";
-$sql = "UPDATE `shop` SET `id`='3' WHERE name = 'billy'";
-$sql = "UPDATE `shop` SET `id`='4' WHERE name = 'gary'";
 
 mysqli_query($con,$sql); 
 
