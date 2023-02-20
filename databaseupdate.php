f7cee701e65aeb7bb9b5411a488fc360e3136f97
SSSSaran <ssaran@imgroup.co.uk>
2023-02-20 15:43:00 +0000

    Squashed commit of the following:
    commit aaed6a54da15cafeaefa1143b250c2b587c83879
    Author: SSSSaran <125583108+SSSSaran@users.noreply.github.com>
    Date:   Mon Feb 20 15:42:04 2023 +0000
    
        Update databaseupdate.php
    
    commit 561c273d286c282d75dd1fccfceb1999684d8293
    Author: SSSSaran <125583108+SSSSaran@users.noreply.github.com>
    Date:   Mon Feb 20 15:40:16 2023 +0000
    
        Update databaseupdate.php
    
    commit a62aa3ce35ce1b38cc9380c61b563ccd85199fb9
    Author: SSSSaran <125583108+SSSSaran@users.noreply.github.com>
    Date:   Mon Feb 20 15:38:49 2023 +0000
    
        Update databaseupdate.php

--- databaseupdate.php
+++ databaseupdate.php
@@ -2,7 +2,7 @@
 
 $con=mysqli_connect("localhost","root","Prabjot","shop");
 
-$sql = "UPDATE `shop` SET `id`='2' WHERE name = 'bob'";
+$sql = "UPDATE `shop` SET `id`= '5' WHERE 'name' = 'gary'";
 
 mysqli_query($con,$sql); 
 
